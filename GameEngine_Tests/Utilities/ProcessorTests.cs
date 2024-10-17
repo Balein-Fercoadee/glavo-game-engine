805e86fa5cc8e13fc06e4371d7372edb4e863e29
Predrag Djukic <pdjukic@gmail.com>
2024-10-17 00:32:13 -0500

    Fixed the Console EOL fixer

--- GameEngine_Tests/Utilities/ProcessorTests.cs
+++ GameEngine_Tests/Utilities/ProcessorTests.cs
@@ -33,12 +33,18 @@ public class ProcessorTests
 
         actualOutput = CleanConsoleOutput(actualOutput);
         Assert.IsFalse(quit);
-        //Assert.AreEqual(expectedOutput, actualOutput);
+        Assert.AreEqual(expectedOutput, actualOutput);
     }
 
     [TestMethod]
     public void NullVerb()
     {
+        GameState gameState = new GameState();
+        Player player = new Player();
+        GameDatabase gameDb = new GameDatabase();
+        gameDb.Load(@"test_data/", "test_game_database.gge", true);
+        gameState.PlayerData = player;
+        gameState.GameData = gameDb;
         bool quit = false;
         // For some reason the actual output appends a carrage return and line feed.
         string expectedOutput = "You don't know how to do that!";
@@ -46,7 +52,7 @@ public class ProcessorTests
         using (StringWriter sw = new StringWriter())
         {
             Console.SetOut(sw);
-            quit = Processor.ProcessWords(null, null, null);
+            quit = Processor.ProcessWords(null, null, gameState);
             sw.Flush();
 
             actualOutput = sw.ToString();
@@ -54,19 +60,19 @@ public class ProcessorTests
 
         actualOutput = CleanConsoleOutput(actualOutput);
         Assert.IsFalse(quit);
-        //Assert.AreEqual(expectedOutput, actualOutput);
+        Assert.AreEqual(expectedOutput, actualOutput);
     }
 
     [ExcludeFromCodeCoverage]
     private string CleanConsoleOutput(string consoleOutput)
     {
-        string cleanOutput;
+        string cleanOutput = consoleOutput;
         // The IF below makes the Console output work regardless of OS
         // WriteLine on Windows uses '\r\n', Linux uses '\n'.
         if (consoleOutput.EndsWith("\r\n"))
-            cleanOutput = consoleOutput[..^3];
-        else // the string ends in '\r\n'
-            cleanOutput = consoleOutput.Substring(0, consoleOutput.Length - 2);
+            cleanOutput = consoleOutput[..^2];
+        else if (consoleOutput.EndsWith("\n")) // the string ends in '\n'
+            cleanOutput = consoleOutput[..^1];
 
         return cleanOutput;
     }
